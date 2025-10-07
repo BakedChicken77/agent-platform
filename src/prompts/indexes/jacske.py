fe39fc179ae26498b20b838c6f6a56f69c361cf9
BakedChicken77 <130414989+BakedChicken77@users.noreply.github.com>
2025-10-07 18:36:10 -0500

    Add JAC program's SKE context to prompts

---
+++ src/prompts/indexes/jacske.py
@@ -0,0 +1,14 @@
+from schema import ProgramContext
+
+
+full_program_name = """Japan Aerospace Corporation (JAC) program's Station Keeping Equipment (SKE)"""
+
+detailed_overview = """\
+SYSTEM OVERVIEW
+TBD
+"""
+
+JACSKE = ProgramContext(
+        full_program_name=full_program_name, 
+        detailed_overview=detailed_overview,
+    )
